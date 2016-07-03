fca47212a7f4bb85e180e10528b0027a0b164762
roy <kusemererwaroy@gmail.com>
2016-07-03 18:11:15 +0300

    more changes to index

--- index.php
+++ index.php
@@ -1,1 +1,273 @@
-Yolo
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Welcome to AGMIS!</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <style>
+        .jumbotron {
+            position: relative;
+            font-family:"Open Sans Light";
+            background: url("img/d.jpg") no-repeat center center;
+            width:100%;
+            height: 65%;
+            background-size: 100% 100%;
+        }
+        .shadow {
+            border-radius:0px;
+            -webkit-box-shadow: 0 0 30px black;
+            -moz-box-shadow: 0 0 30px black;
+            box-shadow: 0 0 30px black;
+        }
+    </style>
+</head>
+
+<body>
+
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top shadow" role="navigation" style="height:70px; background: #373E46; border:0px;">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html">
+                AGMIS
+            </a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="about.html">About</a>
+                </li>
+                <li>
+                    <a href="services.html">Services</a>
+                </li>
+                <li>
+                    <a href="contact.html">Contact</a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Portfolio <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="portfolio-1-col.html">1 Column Portfolio</a>
+                        </li>
+                        <li>
+                            <a href="portfolio-2-col.html">2 Column Portfolio</a>
+                        </li>
+                        <li>
+                            <a href="portfolio-3-col.html">3 Column Portfolio</a>
+                        </li>
+                        <li>
+                            <a href="portfolio-4-col.html">4 Column Portfolio</a>
+                        </li>
+                        <li>
+                            <a href="portfolio-item.html">Single Portfolio Item</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Blog <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="blog-home-1.html">Blog Home 1</a>
+                        </li>
+                        <li>
+                            <a href="blog-home-2.html">Blog Home 2</a>
+                        </li>
+                        <li>
+                            <a href="blog-post.html">Blog Post</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Other Pages <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="full-width.html">Full Width Page</a>
+                        </li>
+                        <li>
+                            <a href="sidebar.html">Sidebar Page</a>
+                        </li>
+                        <li>
+                            <a href="faq.html">FAQ</a>
+                        </li>
+                        <li>
+                            <a href="404.html">404</a>
+                        </li>
+                        <li>
+                            <a href="pricing.html">Pricing Table</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
+</header>
+
+
+<div class="jumbotron">
+<div class="container">
+
+    <div class="col-md-5">
+
+
+    <p style="font-size:50px;">GET AGRIC PRICES</p>
+
+    <div class="panel panel-default shadow" style="border-radius:0px;">
+
+        <div class="container">
+
+        <p>Be the First to know! Get an update when a new report arrives</p>
+        <p>
+            <div class="input-group">
+            <input type="text" class="form-control" placeholder="Enter your Email Address" arialdescribedby="basic-addon2" style="border-radius:0px;">
+            <span class="input-group-addon" id="basic-addon2" style="border-radius:0px; background:#FFB401;">SUBSCRIBE</span>
+        </div>
+        </p>
+
+        </div>
+
+    </div>
+    </div>
+
+</div>
+</div>
+
+
+<div class="container" style=" margin-top:-50px; position: relative;">
+
+    <div class="panel panel-default shadow" style="font-family:'Open Sans';">
+        <div class="panel-body">
+            <img src="img/p.jpg" class="col-md-5">
+            <div class="col-md-7" style="border:0px solid gray;">
+                <h3>Agricultural Information Reporting System</h3>
+                <p class="lead">Globally benchmark interdependent benefits vis-a-vis magnetic leadership skills. Authoritatively productivate future-proof products via empowered channels. Authoritatively syndicate plug-and-play functionalities.</p>
+                <p>Authoritatively pontificate enabled ideas and user friendly ROI. Objectively pontificate granular meta-services and market positioning ideas. Interactively engineer 2.0 manufactured products without sustainable quality vectors. Globally conceptualize parallel growth strategies with.</p>
+                <button class="btn btn-default" style="border-radius:0px;">READ MORE</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!--Footer-->
+
+<section class="contact" style="background:#373E46;min-height:300px;color:#fff; font-family:georgia;">
+    <hr>
+    <div class="container">
+        <div class="col-md-3">
+            <ul style="list-style: none;">
+                <li>Write for Us</li>
+                <li>Books</li>
+                <li>Mentorship</li>
+                <li>Licensing</li>
+            </ul>
+        </div>
+
+        <div class="col-md-3">
+            <ul style="list-style: none;">
+                <li>Write for Us</li>
+                <li>Books</li>
+                <li>Mentorship</li>
+                <li>Licensing</li>
+            </ul>
+        </div>
+
+
+        <div class="col-md-2">
+            <ul style="list-style: none;">
+                <li>Write for Us</li>
+                <li>Books</li>
+                <li>Mentorship</li>
+                <li>Licensing</li>
+            </ul>
+        </div>
+
+
+
+        <script>(function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=727084914091193";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+
+
+        <div class="col-md-4">
+
+            <div class="fb-page"
+                 style="margin-left:0px;width:100%;";
+                 data-href="https://www.facebook.com/evolutionmediagroupug"
+                 data-width="380"
+                 data-hide-cover="false"
+                 data-show-facepile="true"
+                 data-show-posts="false"> Facebook Page Loading...</div>
+
+        </div>
+
+
+
+        <style>
+            #social-icons
+            {
+                font-size:40px;
+            }
+            #social-icons #content
+            {
+                margin-top:20px;
+            }
+        </style>
+
+        <div class="col-md-4 text-center center-block" id="social-icons">
+            <div id="content">
+                <i class="fa fa-facebook-square"></i>
+                <i class="fa fa-twitter-square"></i>
+                <i class="fa fa-linkedin-square"></i>
+                <i class="fa fa-google-plus-square"></i>
+                <i class="fa fa-globe"></i>
+
+                <hr color="#fff">
+                <p style="font-size:20px;">&copy; AGMIS <?php echo date('Y'); ?></p>
+            </div>
+        </div>
+
+
+
+
+
+
+<script src="js/jquery.js" type="text/javascript"></script>
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
--- readme.txt
+++
@@ -1,4 +0,0 @@
-Hello
-Roy
-Lilian
-ADash
